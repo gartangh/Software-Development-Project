f2361a988d6e40008310ceaf8ba00012d76a9e2c
Hannes Demuynck <Hannes.Demuynck@UGent.be>
2018-04-18 19:14:54 +0200

    tested quizroom

--- src/server/ServerContext.java
+++ src/server/ServerContext.java
@@ -148,8 +148,8 @@ public class ServerContext {
 	}
 
 	public void loadData() {
-		BufferedReader reader;// ./Files/
-		String locationPrefix = "D:\\Documents\\Universiteit\\Bachelor3\\Softwareontwikkeling\\project-1718-groep9\\src\\server\\";
+		BufferedReader reader;// ./Files/ //"d:\Demuynck\Documents\school\Softwareontwikkeling\Project\project-1718-groep9\src\server"
+		String locationPrefix = "d:\\Demuynck\\Documents\\school\\Softwareontwikkeling\\Project\\project-1718-groep9\\src\\server\\";
 		String[] themeFiles = { "QUESTIONS_CULTURE.txt", "QUESTIONS_SPORTS.txt" };
 		try {
 			for (int tF = 0; tF < themeFiles.length; tF++) {
